3f024cef1f4e811ea5ca0f314d99d5f321da047e
raf1n <rafinc10@gmail.com>
2022-11-24 13:29:58 +0600

    footer-added

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../../Pages/Shared/Footer/Footer";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="max-w-[1440px] mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
